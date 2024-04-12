0de75c67eeebaf2355dddc96eabd07d2f2764ee6
celesteorellana1 <sintiaorellana1@gmail.com>
2024-04-12 16:30:57 -0600

    primer commit

---
+++ Desafio1HerenciaCalculadora/Desafio1HerenciaCalculadora/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1HerenciaCalculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Calculadora calc = new Calculadora("Casio", "fx-50");
+            Console.WriteLine($"Suma: {calc.Sumar(15, 20)}");
+            Console.WriteLine($"Resta: {calc.Restar(30, 16)}");
+            Console.WriteLine($"Multiplicacion: {calc.Multiplicar(5, 25)}");
+            Console.WriteLine($"Division: {calc.Dividir(80,5)}");
+
+            CalculadoraCientifica cientifica = new CalculadoraCientifica("Xiaomi", "CalX");
+            Console.WriteLine($"Potencia: {cientifica.Potencia(2, 3)}");
+            Console.WriteLine($"Raiz: {cientifica.Raiz(16)}");
+            Console.WriteLine($"Modulo: {cientifica.Modulo(20, 3)}");
+            Console.WriteLine($"Logaritmo: {cientifica.Logaritmo(100, 10)}");
+
+            double n1 = 128;
+            double n2 = 8;
+            double total = calc.Sumar(n1, n2);
+            Console.WriteLine(total);
+            double diferencia = calc.Restar(n1, n2);
+            Console.WriteLine(diferencia);
+
+            double raiz = cientifica.Raiz(n1);
+            Console.WriteLine(raiz);
+
+            MostrarTipo(calc);
+            MostrarTipo(cientifica);
+
+            Console.ReadLine();
+        }
+
+        public static void MostrarTipo(Calculadora calculadora)
+        {
+            Console.WriteLine(calculadora .Tipo());
+        }
+    }
+
+    public class Calculadora
+    {
+        public Calculadora()
+        {
+        }
+
+        public string Marca { get; set; }
+        public string Serie { get; set;}
+
+        public Calculadora(string marca, string serie)
+        {
+            Marca = marca;
+            Serie = serie;
+        }
+
+        public double Sumar(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public double Restar(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public double Multiplicar(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public double Dividir(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+
+        public string Tipo()
+        {
+            return "Calculadora Basica";
+        }
+    }
+
+    public class CalculadoraCientifica : Calculadora
+    {
+        public CalculadoraCientifica(string marca, string serie) : base(marca, serie)
+        {
+        }
+
+        public double Potencia(double n1, double n2)
+        {
+            return Math.Pow(n1, n2);
+        }
+
+        public double Raiz(double n1)
+        {
+            return Math.Sqrt(n1);
+        }
+
+        public virtual string Tipo()
+        {
+            return "Cientifica";
+        }
+        
+        public double Modulo(double n1, double n2)
+        {
+            return n1 % n2;
+        }
+
+        public double Logaritmo(double n1, double n2)
+        {
+            return Math.Log(n1, n2);
+        }
+
+    }
+}
---
+++ Desafio2Notificaciones/Desafio2Notificaciones/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio2Notificaciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            INotificable notificadorEmail = new NotificacionEmail("sorellanag3@miumg.edu.gt");
+            notificadorEmail.Notifica(" Hola,soy Sinthia");
+            
+            INotificable notificadorWhatsapp = new NotificacionWhatsap("+502 5905-4810");
+            notificadorWhatsapp.Notifica("He enviado un correo");
+         
+            INotificable notificadorSMS = new NotificacionSMS("+502 3456-5766");
+            notificadorSMS.Notifica("Me gustan los k-dramas");
+
+            Console.ReadLine();
+        }
+    }
+    public interface INotificable
+    {
+        void Notifica(string mensaje);
+    }
+
+    public class NotificacionEmail : INotificable
+    {
+        public string DirreccionCorreo { get; set; }
+        public NotificacionEmail(string dirreccionCorreo)
+        {
+            DirreccionCorreo = dirreccionCorreo;
+        }
+
+        public void Notifica(string mensaje)
+        {
+            Console.WriteLine($"Enviando Email a {DirreccionCorreo}:{mensaje}");
+        }
+    }
+
+    public class NotificacionWhatsap : INotificable
+    {
+        public string NumeroTelefono { get; set; }
+        public NotificacionWhatsap(string numeroTelefono)
+        {
+            NumeroTelefono = numeroTelefono;
+        }
+
+        public void Notifica( string mensaje)
+        {
+            Console.WriteLine($"Enviando WhatsApp al {NumeroTelefono}: {mensaje}");
+        }
+    }
+
+    public class NotificacionSMS : INotificable
+    {
+        public string NumeroTelefono { get; set; }
+        public NotificacionSMS(string numeroTelefono)
+        {
+            NumeroTelefono = numeroTelefono;
+        }
+
+        public void Notifica(string mensaje)
+        {
+            Console.WriteLine($"Enviando SMS al {NumeroTelefono}: {mensaje}");
+        }
+    }
+}
